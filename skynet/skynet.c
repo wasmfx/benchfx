5432837fd3b397ff7e5512475ea4a54b0bec2a72
Daniel Hillerström <daniel.hillerstrom@ed.ac.uk>
2023-08-21 20:36:20 +0200

    Start shared version

---
+++ skynet/skynet.c
@@ -0,0 +1,33 @@
+// WasmFX implementation of the Skynet benchmark.  This implementation
+// does not use any form of coroutines.
+
+#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+#include "parameters.h"
+
+extern void yield(uint64_t);
+extern uint64_t handle(uint32_t, uint64_t);
+
+__attribute__((export_name("skynet")))
+uint64_t skynet(uint32_t level, uint64_t num) {
+  if (level == 0) {
+    yield(num);
+    return -1; // dummy value
+  }
+  else {
+    uint64_t lb = num * children_per_level;
+    uint64_t ub = lb + children_per_level;
+    uint64_t sum = 0;
+    level--;
+    for (uint64_t i = lb; i < ub; i++)
+      sum += handle(level, i);
+    return sum;
+  }
+}
+
+int main(void) {
+  printf("%" PRIu64 "\n", skynet(num_levels, 0));
+  return 0;
+}
