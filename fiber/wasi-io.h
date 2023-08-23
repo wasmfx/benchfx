0cafad12d287f5c9a70b9e5c2c576b86e9d85047
Daniel Hillerström <daniel.hillerstrom@ed.ac.uk>
2023-08-23 19:36:44 +0200

    Give credit

--- fiber/wasi-io.h
+++ fiber/wasi-io.h
@@ -2,17 +2,22 @@
 #define __WASI_IO_H
 
 #include <stdint.h>
-#include <wasi/api.h>
 #include <unistd.h>
+#include <wasi/api.h>
 
-#define wasi_print(msg) do { \
-    const uint8_t *msg_start = (uint8_t *)msg; \
-    const uint8_t *msg_end = msg_start; \
-    for (; *msg_end != '\0'; msg_end++) {} \
-    __wasi_ciovec_t iov = {.buf = msg_start, .buf_len = msg_end - msg_start}; \
+// NOTE: We cannot use printf() and most friends because asyncify
+// attempts to transform them and inadvertently ending up corrupting
+// its own state resulting in an "unreachable" error at runtime.
+// The following code is adapted from
+//   https://gist.github.com/s-macke/6dd78c78be46214d418454abb667a1ba
+#define wasi_print(str) do { \
+    const uint8_t *start = (uint8_t *)str; \
+    const uint8_t *end = start; \
+    for (; *end != '\0'; end++) {} \
+    __wasi_ciovec_t iov = {.buf = start, .buf_len = end - start}; \
     size_t nwritten; \
     (void)__wasi_fd_write(STDOUT_FILENO, &iov, 1, &nwritten);   \
 } while (0)
-#define wasi_print_debug(msg) \
-    wasi_print(__FILE__ ":" STRINGIZE(__LINE__) ": " msg "\n")
+#define wasi_print_debug(str) \
+    wasi_print(__FILE__ ":" STRINGIZE(__LINE__) ": " str "\n")
 #endif
