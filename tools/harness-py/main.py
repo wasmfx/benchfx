81fcd4c8860c46e176c19634ddb19c208ec5fb81
Frank Emrich <git@emrich.io>
2024-02-21 14:13:59 +0000

    WIP

--- tools/harness-py/main.py
+++ tools/harness-py/main.py
@@ -1,6 +1,7 @@
 import argparse
 import subprocess
-
+import multiprocessing
+import math
 import config
 
 from typing import List, Tuple
@@ -15,6 +16,9 @@ ExitCode = int
 
 REPOS_PATH="tools/repos"
 SPEC_REPO="spec"
+BINARYEN_REPO="binaryen"
+WASMTIME_REPO1="wasmtime1"
+WASMTIME_REPO2="wasmtime2"
 
 
 @dataclass
@@ -36,28 +40,55 @@ def check(condition, msg):
     if not condition:
         raise HarnessError(msg)
 
-def log(msg):
-    print(msg)
+def log(msg, sep = None):
+    print(msg, sep=sep)
+
+SHOW_OUTPUT=True
 
 # def run_args(cmd, cwd=None) -> Tuple[int, str, str]:
 #     completed = subprocess.run(cmd, cwd=cwd, capture_output=True, shell=True)
 #     return (completed.returncode, completed.stdout.decode('utf-8'), completed.stderr.decode('utf-8'))
 
-def run(cmd, cwd = None):
-    completed = subprocess.run(cmd, cwd=cwd, capture_output=True, shell=True)
-    return (completed.returncode, completed.stdout.decode('utf-8'), completed.stderr.decode('utf-8'))
+# class ProcessOutput:
+#     def __init__(self, cp : subprocess.CompletedProcess):
+#         self.returncode = cp.returncode
+#         self.stdout =
+
+def run(cmd, cwd = None) -> subprocess.CompletedProcess:
+    log(f"cmd is {cmd}, cwd is {cwd}")
+    res = subprocess.run(cmd, cwd=cwd, capture_output=True, shell=True, text = True)
+    if SHOW_OUTPUT:
+        log(res.stdout, sep = "")
+        log(res.stderr, sep = "")
+    return res
 
 
-
-def run_check(cmd, msg, cwd = None):
-    print(f"cmd is {cmd}, cwd is {cwd}")
+# Like run, but checks that the command finished with non-zero exit code.
+def run_check(cmd, msg = None, cwd = None):
     result = run(cmd, cwd)
-    check(result[0] == 0, msg + f"Details:\nCommand failed: {cmd}\nStdout: {result[1]}, Stderr: {result[2]}")
+    msg = msg or f"Running {cmd} in {cwd or os.getcwd()} failed"
+    check(result.returncode == 0, msg + f"\nDetails:\nCommand failed: {cmd}\nStdout: {result.stdout}, Stderr: {result.stderr}")
+    return result
 
 
 class Binaryen:
+
     def __init__(self, path: Path):
-        self.path = Path
+        self.path = path
+
+    def build(self):
+        cpus = math.ceil(multiprocessing.cpu_count() / 2)
+        pass
+
+
+        #run_check("mkdir -p out", cwd = selfpath)
+        run_check("cmake .", msg = "cmake for binaryen failed", cwd = self.path)
+        run_check(f"make -j {cpus}", msg = "building binaryen failed", cwd = self.path)
+
+
+    def wasm_merge_path(self) -> str:
+        return str(os.path.join(self.path, "bin", "wasm-merge"))
+
 
 
 class RefeferenceInterpreter:
@@ -67,12 +98,18 @@ class RefeferenceInterpreter:
     def build(self):
         run_check("make", "Failed to build reference interpreter", self.path)
 
-    def assemble(self, input_wast_path, output_wasm_path):
-        run_check(f"wasm -d '{input_wast_file}' -o '{output_wasm_path}'", self.path)
+    def compile(self, input_path, output_path):
+        run_check(f"wasm -d '{input_path}' -o '{output_path}'", self.path)
 
 
 
+class Wasmtime:
+    def __init__(self, path : Path):
+        self.path = path
 
+    def build(self):
+        pass
+        #run_check("make", "Failed to build reference interpreter", self.path)
 
 
 
@@ -81,19 +118,39 @@ class RefeferenceInterpreter:
 class GitRepo:
     def __init__(self, path):
         self.path = path
+
+        check(GitRepo.is_root_of_repo_or_worktree(path),
+              f"{path} is not the root of a git repository (or a worktree of a repository)")
+
         self.repo = git.Repo(path)
 
+
+    @staticmethod
+    def is_root_of_repo_or_worktree(path):
+        res = run("git rev-parse --show-toplevel", cwd = path)
+        if res.returncode != 0:
+            return False
+        toplevel_path = res.stdout.strip()
+        return Path(toplevel_path).absolute() == Path(path).absolute()
+
+
+    # This uses run_check, use only for git commands that are not allowed to fail
     def git(self, args):
-        return run_args("git", args.split(), cwd=self.path)
+        return run_check("git " + args, cwd=self.path)
 
 
-    def is_dirty(self):
-        return self.repo.is_dirty()
+    def is_dirty(self, allow_untracked = True):
+        untracked_mode = "no" if allow_untracked else "normal"
+        res = self.git(f"status --untracked-files={untracked_mode} --porcelain")
+        # the --porcelain option makes the output stable, where no output means clean repository
+        return res.stdout.strip() != ""
 
 
+    # TODO Do we always need to also update submodules?
     def checkout(self, revision):
-        check(not self.repo.is_dirty(), f"Cannot checkout git repo at {self.path} to {revision} because it is dirty")
-        self.repo.git.checkout(revision)
+        check(not self.is_dirty(), f"Cannot checkout git repo at {self.path} to {revision} because it is dirty")
+        self.git(f"switch --detach {revision}")
+        self.git("submodule update --init --recursive")
 
 
 
@@ -110,6 +167,8 @@ class Run:
     def execute(args):
         print("run is running")
 
+        # Reference interpreter setup
+
         spec_repo_path = os.path.join(REPOS_PATH, SPEC_REPO)
         log(f"spec repo expected at {spec_repo_path}")
 
@@ -124,6 +183,35 @@ class Run:
 
         interpreter.build()
 
+
+        # Binaryen setup
+        binaryen_repo_path = os.path.join(REPOS_PATH, BINARYEN_REPO)
+        log(f"binaryen repo expected at {binaryen_repo_path}")
+
+        binaryen_repo = GitRepo(binaryen_repo_path)
+
+        log(f"binaryen repo dirty? {binaryen_repo.is_dirty()}")
+
+        binaryen_repo.checkout(config.BINARYEN_COMMIT)
+
+        binaryen = Binaryen(binaryen_repo_path)
+
+        binaryen.build()
+
+
+        # Wasmtime setup
+        wasmtime_repo_path = os.path.join(REPOS_PATH, WASMTIME_REPO1)
+        log(f"wasmtime repo expected at {wasmtime_repo_path}")
+
+        wasmtime_repo = GitRepo(wasmtime_repo_path)
+
+        log(f"wasmtime repo dirty? {wasmtime_repo.is_dirty()}")
+
+        wasmtime_repo.checkout(config.WASMTIME_COMMIT)
+
+        wasmtime = Wasmtime(wasmtime_repo_path)
+
+        wasmtime.build()
 
 
     @staticmethod
